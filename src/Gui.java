ce348efa344195d41d1addcca7d4f2dc75cffb2f
ggf554 <ggf554@hotmail.com>
2018-07-12 04:15:46 +0100

    yeah

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.FileDialog;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Panel;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
@@ -6,7 +11,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,17 +32,69 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 
 	int startX, endX, startY, endY;
 
+	Vector wireInfo = null;//set of vector information for wires
+	Pointer endFlag = new Pointer(-1, -1, -1);
+	
+	FileInputStream picIn = null;
+	FileOutputStream picOut = null;
+	ObjectInputStream OIn = null;
+	ObjectOutputStream OOut = null;
+	
 	JPanel p1;
-
+	JPanel toolPanel;
+	
+	JButton openPic, savePic; //save or load the picture
+	FileDialog openPicture, savePicture;
+	
+	
 	Component c = new Component();
 
 	public Gui() {
+		super();
+		
+		wireInfo = new Vector();
+		
+		toolPanel = new JPanel();
+		openPic = new JButton("Open file");
+		savePic = new JButton("Save file");
+		
+		openPic.addActionListener(this);
+		savePic.addActionListener(this);
+		
+		openPicture = new FileDialog(this, "open file", FileDialog.LOAD);
+		savePicture = new FileDialog(this, "save file", FileDialog.SAVE);
+		openPicture.setVisible(false);
+		savePicture.setVisible(false);
+		openPicture.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				openPicture.setVisible(false);
+			}
+		});
+		savePicture.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				savePicture.setVisible(false);
+			}
+		});
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
+		toolPanel.add(openPic);
+		toolPanel.add(savePic);
+		
+		
+		this.add(toolPanel,BorderLayout.NORTH);
+		
+		
 		p1 = new JPanel();
 
 		this.setTitle("Drawing things");
 		this.add(p1);
 		this.addMouseListener(this);
-		this.setSize(400, 400);
+		this.setSize(900, 600);
 		this.setVisible(true);
 		this.setLocation(200, 100);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -35,6 +103,24 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 
 	}
 
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Pointer p1,p2;
+		int n = wireInfo.size();
+		for(int i=0; i<n; i++) {
+			p1 = (Pointer) wireInfo.elementAt(i);
+			p2 = (Pointer) wireInfo.elementAt(i+1);
+			System.out.println("!!"+p1.x);
+			Line2D line2 = new Line2D.Double(p1.x, p1.y, p2.x, p2.y);
+			g2d.draw(line2);
+		}
+		
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}
+	
 	public boolean mouseIn(int x, int y) {
 
 		if (x >= c.getX() && x <= c.getX() + c.getWidth() && y >= c.getY() && y <= c.getY() + c.getHight()) {
@@ -50,7 +136,49 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getSource() == openPic) {//open the picture
+			openPicture.setVisible(true);
+			if (openPicture.getFile() != null) {
+				
+				repaint();
+				try {
+					wireInfo.removeAllElements();
+					File filein = new File(openPicture.getDirectory(), openPicture.getFile());
+					picIn = new FileInputStream(filein);
+					OIn = new ObjectInputStream(picIn);
+					wireInfo = (Vector) OIn.readObject();
+					OIn.close();
+					repaint();
+					
+				} catch (ClassNotFoundException IOe2) {
+					repaint();
+					
+					System.out.println("can not read object");
+				} catch (IOException IOe) {
+					repaint();
+					
+					System.out.println("can not read file");
+				}
+			}
+		
+			
+		}
+		
+		if (e.getSource() == savePic) {//save the picture
+				savePicture.setVisible(true);
+				try {
+					File fileout = new File(savePicture.getDirectory(), savePicture.getFile());
+					picOut = new FileOutputStream(fileout);
+					OOut = new ObjectOutputStream(picOut);
+					OOut.writeObject(wireInfo);
+					OOut.close();
+				} catch (IOException IOe) {
+					System.out.println("can not write object");
+				}
+			
+		}
 
+		
 	}
 
 	int dragx, dragy;
@@ -60,6 +188,14 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
 
+//		Pointer p1;
+//		int x = (int) e.getX();
+//		int y = (int) e.getY();
+//		p1 = new Pointer(x, y, 1);
+//		wireInfo.addElement(p1);
+//		System.out.println(wireInfo);
+		
+		
 		if (mouseIn(e.getX(), e.getY())) {
 
 			c.cleanComponent(g);
@@ -74,10 +210,12 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 			c.setY(e.getY() - dragy);
 
 			c.paintComponent(g);
+			
 		} else {
 			first = true;
 		}
 
+		
 	}
 
 	@Override
@@ -92,9 +230,17 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 
 	}
 
+	int x,y;
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
+		Pointer p2;
+		x = (int) e.getX();
+		y = (int) e.getY();
+		p2 = new Pointer(x, y, 1);
+		wireInfo.addElement(p2);
+	
+		
 		if (!mouseIn(e.getX(), e.getY())) {
 			startX = e.getX();
 			startY = e.getY();
@@ -105,7 +251,14 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		Pointer p3;
+		x = (int) e.getX();
+		y = (int) e.getY();
+		p3 = new Pointer(x, y, 1);
+		wireInfo.addElement(p3);
+		System.out.println(wireInfo);
+		repaint();
+		
 		if (!mouseIn(e.getX(), e.getY())) {
 			endX = e.getX();
 			endY = e.getY();
@@ -125,7 +278,7 @@ public class Gui extends JFrame implements MouseListener, MouseMotionListener, A
 				c.setY(endY);
 				c.setHight(startY - endY);
 			}
-
+			
 			c.paintComponent(g);
 		}
 
